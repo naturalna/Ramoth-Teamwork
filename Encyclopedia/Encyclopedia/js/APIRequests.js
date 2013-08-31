78d8421d756376bb47e47ba9003b3675c386754b
naturalna <naturalna89@gmail.com>
2013-08-31 11:34:44 +0300

    empty request namespace add

--- Encyclopedia/Encyclopedia/js/APIRequests.js
+++ Encyclopedia/Encyclopedia/js/APIRequests.js
@@ -2,13 +2,13 @@
 
     var getProvider = function () { };
     var getHierarchies = function () { };
-    var queryForFishesSearch = function () {
+    var queryForFishesSearch = function (page) {
         //json
     };
 
-    return {
+    WinJS.Namespace.define("APIRequests", {
         getProvider: getProvider,
         getHierarchies: getHierarchies,
         queryForFishesSearch: queryForFishesSearch,
-    }
+    });
 }());
--- Encyclopedia/Encyclopedia/js/dataLayer.js
+++ Encyclopedia/Encyclopedia/js/dataLayer.js
@@ -1,14 +1,14 @@
 ﻿//here we have all requests to db
 (function () {
     //this must be in separeted class
-    var downloadRequest = function () {
-        //TODO
-        //in response 1 page (30)
-        //return jsonObjec with name discription and image url.It may contains something else
-        return [{ name: "testName", discription: "tessssssssssssssst", imageURL: "" }];
+    //var downloadRequest = function () {
+    //    //TODO
+    //    //in response 1 page (30)
+    //    //return jsonObjec with name discription and image url.It may contains something else
+    //    return [{ name: "testName", discription: "tessssssssssssssst", imageURL: "" }];
         
-    };
-
+    //};
+    var downloaded = APIRequests.queryForFishesSearch(1);
     var allFishes = [];
 
     var generateModels = function () {
