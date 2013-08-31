13b1a8cce08d6ec7b2ba4478d7b5192211150020
Boris Penev <borispenev89@gmail.com>
2013-08-31 15:59:09 -0700

    Repaired the Api requests
    and added an example of the usage of search request in the default.js

---
+++ Encyclopedia/Encyclopedia/bin/Debug/AppX/js/models.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    //here are DTO objects
+    //TODO check for more info in json
+    var FishModel =
+        WinJS.Class.define(function (name, discription, imageURL) {
+            this.name = name;
+            this.discription = discription;
+            this.imageURL = imageURL;
+        }, {
+            name: "",
+            discription: "",
+            imageURL :"",
+        })
+
+    var AllFishes = function()
+    {
+        return Data.allFishes;
+    }
+
+    WinJS.Namespace.define("Models", {
+        FishModel: FishModel,
+        AllFishes: AllFishes,
+    })
+})();
---
+++ Encyclopedia/Encyclopedia/bin/Debug/AppX/js/viewmodels.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var fishList = new WinJS.Binding.List([]);
+
+    var loadFish = function () {
+        var fishDTOs = Data.getFishes();
+
+        var currentCount = fishList.dataSource.list.length
+        fishList.dataSource.list.splice(0, currentCount);
+
+        for (var i = 0; i < fishDTOs.length; i++) {
+            fishList.push(fishDTOs[i]);
+        }
+    }
+
+    WinJS.Namespace.define("ViewModels", {
+        loadFish: loadFish,
+        fishes: fishList,
+        //addFish: function (name, discription, imageURL) {
+        //    Data.addFish(new Models.FishModel(name, discription, imageURL));
+        //}
+    });
+})();
